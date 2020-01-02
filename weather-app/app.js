ec7a952ee04215d4d930208878cc15d6acaf1455
Subramaniam <subramaniam.vls@gmail.com>
2020-01-02 17:28:36 +0530

    02-DEC-2020

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,15 +5,15 @@ const address = process.argv[2];
 if (!address) {
   console.log('Please provide an address!');
 } else {
-  geoCode('Chennai', (error, data) => {
+  geoCode('Chennai', (error, { longitude, latitude, location }) => {
     if (error) {
       return console.log(error);
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error);
       }
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     });
   });
