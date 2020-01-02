8baf9fa6101762e0865df4f95fd53da36c64e22e
Subramaniam <subramaniam.vls@gmail.com>
2020-01-02 20:29:36 +0530

    02-DEC-2020

---
+++ web-server/src/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Subramaniam'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Subramaniam'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Subramaniam'
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('pagenotfound', {
+    error: 'Error - Cannot find the help article',
+    name: 'Subramaniam'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('pagenotfound', {
+    error: 'Error 404 - Page not found',
+    name: 'Subramaniam'
+  });
+});
+
+app.listen('3000', () => {
+  console.log('Server started at port 3000!');
+});
