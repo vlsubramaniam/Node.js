7d956a1f733bef7b9f89c6e2ce96015f76709e1b
Subramaniam <subramaniam.vls@gmail.com>
2020-01-03 18:23:37 +0530

    03-JAN-2020

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,47 +1,48 @@
-const express = require('express')
-const path = require('path')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
-const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
     name: 'Subramaniam VL'
-  })
-})
+  });
+});
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
     name: 'Subramaniam VL'
-  })
-})
+  });
+});
 
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
     name: 'Subramaniam VL'
-  })
-})
+  });
+});
 
 app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
       error: 'You must provide an address!'
-    })
+    });
   }
   geocode(
     req.query.address,
@@ -49,41 +50,41 @@ app.get('/weather', (req, res) => {
       if (error) {
         return res.send({
           error
-        })
+        });
       }
 
       forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
           return res.send({
             error
-          })
+          });
         }
         res.send({
           forecast: forecastData,
           location,
           address: req.query.address
-        })
-      })
+        });
+      });
     }
-  )
-})
+  );
+});
 
 app.get('/help/*', (req, res) => {
   res.render('pagenotfound', {
     title: 'Error 404',
     errorMessage: 'Cannot find the help article',
     name: 'Subramaniam VL'
-  })
-})
+  });
+});
 
 app.get('*', (req, res) => {
   res.render('pagenotfound', {
     title: 'Error 404',
     errorMessage: 'Page not found',
     name: 'Subramaniam VL'
-  })
-})
+  });
+});
 
-app.listen('3000', () => {
-  console.log('Server started at port 3000!')
-})
+app.listen(port, () => {
+  console.log('Server started at port ' + port);
+});
