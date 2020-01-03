b59ca760cb8334cf45967ba76fe18fd23db4bd36
Subramaniam <subramaniam.vls@gmail.com>
2020-01-03 19:18:05 +0530

    03-JAN-2020

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,21 +1,21 @@
-const formElement = document.querySelector('form');
-const inputElement = document.querySelector('input');
-const message1 = document.querySelector('#message-1');
-const message2 = document.querySelector('#message-1');
-message1.textContent = '';
+const formElement = document.querySelector('form')
+const inputElement = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+message1.textContent = ''
 
 formElement.addEventListener('submit', e => {
-  e.preventDefault();
-  const address = inputElement.value;
-  message1.textContent = 'Loading...';
+  e.preventDefault()
+  const address = inputElement.value
+  message1.textContent = 'Loading...'
   fetch('/weather?address=' + address).then(data => {
     data.json().then(res => {
       if (res.error) {
-        message1.textContent = res.error;
+        message1.textContent = res.error
       } else {
-        message1.textContent = res.location;
-        message2.textContent = res.forecast;
+        message1.textContent = res.location
+        message2.textContent = res.forecast
       }
-    });
-  });
-});
+    })
+  })
+})
