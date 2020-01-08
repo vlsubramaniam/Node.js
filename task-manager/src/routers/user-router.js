f8076c96d1cd97ad9364208536acd6d77a890d8e
Subramaniam <subramaniam.vls@gmail.com>
2020-01-08 21:35:28 +0530

    08-01-2020

---
+++ task-manager/src/routers/user-router.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const User = require('../models/user');
+const router = new express.Router();
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send();
+  } catch (err) {
+    res.status(400).send(err);
+  }
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch(err => {
+  //     res.status(400).send(err);
+  //   });
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch('/users/:id', async (req, res) => {
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const updates = Object.keys(req.body);
+  const isValidUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.send(400).send({ error: 'Inavlid updates' });
+  }
+
+  try {
+    const _id = req.params.id;
+    const options = {
+      new: true,
+      runValidators: true
+    };
+    const user = await User.findByIdAndUpdate(_id, req.body, options);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
