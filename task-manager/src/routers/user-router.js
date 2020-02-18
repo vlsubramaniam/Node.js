6e1331df4820e4916f56f1b101463d0b572911c3
Subramaniam <subramaniam.vls@gmail.com>
2020-02-18 12:34:35 +0530

    18-FEB-20

--- task-manager/src/routers/user-router.js
+++ task-manager/src/routers/user-router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
-    res.status(201).send();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -34,10 +36,9 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    res.send(req.user);
   } catch (err) {
     res.status(500).send(err);
   }
@@ -48,7 +49,7 @@ router.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
     const user = await User.findById(_id);
     if (!user) {
-      return res.status(404).send();
+      return res.status(404).send('User Not Found!!!');
     }
     res.send(user);
   } catch (err) {
