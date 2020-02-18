8ba65867435d638ccdb063a76e1626b50861f025
Subramaniam <subramaniam.vls@gmail.com>
2020-02-18 21:34:17 +0530

    18-FEB-20

--- task-manager/src/routers/task-router.js
+++ task-manager/src/routers/task-router.js
@@ -19,11 +19,30 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 router.get('/tasks', auth, async (req, res) => {
+  const match = {},
+    sort = {};
   try {
     // const tasks = await Task.find({});
 
     // const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate('tasks').execPopulate();
+    if (req.query.completed) {
+      match.completed = req.query.completed === 'true';
+    }
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(':');
+      sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
+      })
+      .execPopulate();
     // res.send(tasks);
     res.send(req.user.tasks);
   } catch (err) {
