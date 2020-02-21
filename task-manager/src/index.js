5b92e61f1a2d90613c81409c327ca788a08a09fb
Subramaniam <subramaniam.vls@gmail.com>
2020-02-21 18:06:14 +0530

    21-FEB-20

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routers/user-router');
 const taskRouter = require('./routers/task-router');
 
 const app = express();
-const port = process.env;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
