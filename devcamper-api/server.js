18d10d9a06f4cf170648d27ea02c7e50cdc92b13
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-24 23:39:42 +0530

    24-JUN-2023

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// const logger = require('./middleware/logger');
+const morgon = require('morgan');
 const bootcamps = require('./routes/bootcamps');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgon('dev'));
+}
+// app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
