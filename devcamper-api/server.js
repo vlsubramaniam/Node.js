e374df61fdf533d779be5e02d3c51c4c3e017288
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-24 23:25:39 +0530

    24-JUN-2023

---
+++ devcamper-api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const bootcamps = require('./routes/bootcamps');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `)
+);
