631f81a27ff3e95a5a850650f04f14ce736c719b
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-28 18:07:11 +0530

    28-JUN-2023

--- devcamper-api/routes/bootcamps.js
+++ devcamper-api/routes/bootcamps.js
@@ -11,13 +11,19 @@ const {
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
+const advancedResults = require('../middleware/advancedResults');
+const Bootcamp = require('../models/Bootcamp');
+
 // Include other resources routers
 const courseRouter = require('./courses');
 
 // Re-route into other resource router
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
