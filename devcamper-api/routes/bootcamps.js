6c1613f651a18a289760cb99cfeb8ff81f17411a
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-28 22:26:32 +0530

    28-JUN-2023

--- devcamper-api/routes/bootcamps.js
+++ devcamper-api/routes/bootcamps.js
@@ -17,19 +17,23 @@ const Bootcamp = require('../models/Bootcamp');
 // Include other resources routers
 const courseRouter = require('./courses');
 
+const { protect, authorize } = require('../middleware/auth');
+
 // Re-route into other resource router
 router.use('/:bootcampId/courses', courseRouter);
 
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const cookieParser = require('cookie-parser');
 const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -23,6 +24,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
