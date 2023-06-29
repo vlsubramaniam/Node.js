81dd40625a41f0279d38d4d341f8c6a78859b794
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-29 21:57:21 +0530

    29-JUN-2023

--- devcamper-api/routes/auth.js
+++ devcamper-api/routes/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
 const router = express.Router();
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updateDetails', protect, updateDetails);
+router.put('/updatePassword', protect, updatePassword);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
