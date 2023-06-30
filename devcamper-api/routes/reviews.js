e527303b2f3d044c14ffe94bc3f95fc44e50868b
vlsubramaniam <subramaniam.vls@gmail.com>
2023-06-30 13:59:25 +0530

    30-JUN-2023

--- devcamper-api/routes/reviews.js
+++ devcamper-api/routes/reviews.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: ['name', 'description'],
-  }),
-  getReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: ['name', 'description'],
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
+
+router.route('/:id').get(getReview);
+
 //   .post(protect, authorize('publisher', 'admin'), addCourse);
-router;
+
 //   .route('/:id')
 //   .get(getCourse)
 //   .put(protect, authorize('publisher', 'admin'), updateCourse)
--- devcamper-api/seeder.js
+++ devcamper-api/seeder.js
@@ -8,9 +8,11 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 mongoose.connect(process.env.MONGO_URI);
 
+// Read JSON files
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
@@ -23,12 +25,17 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log('Data imported...'.green.inverse);
     process.exit(0);
@@ -43,6 +50,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log('Data deleted...'.red.inverse);
     process.exit(0);
